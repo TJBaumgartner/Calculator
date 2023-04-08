36781cc6fddcb1c42c0b58eaf7e4e8b22564b1eb
TJBaumgartner <tnorth6@gmail.com>
2023-04-08 12:29:02 -0700

    created another display for output

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const numberButtons = document.querySelector('[data-number]');
+const operatorButtons = document.querySelector('[data-operator]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearEntryButton = document.querySelector('[data-clear-entry]');
+const allClearButton = document.querySelector('[data-all-clear]');
