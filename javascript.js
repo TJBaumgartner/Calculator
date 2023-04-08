de9698cf0904a93bb6f9406adee5f22bea04f0bc
TJBaumgartner <tnorth6@gmail.com>
2023-04-08 13:46:16 -0700

    operator and number buttons update displays

--- javascript.js
+++ javascript.js
@@ -1,5 +1,46 @@
-const numberButtons = document.querySelector('[data-number]');
-const operatorButtons = document.querySelector('[data-operator]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
 const equalsButton = document.querySelector('[data-equals]');
 const clearEntryButton = document.querySelector('[data-clear-entry]');
 const allClearButton = document.querySelector('[data-all-clear]');
+const displayOutput = document.getElementById('displayOutput');
+const displayCurrent = document.getElementById('displayCurrent');
+let perviousNumber = '';
+let currentNumber = '';
+let currentOperation = null;
+let operatorActive = false;
+
+
+
+function setNumber(number){
+    if(displayCurrent.textContent == 0){
+        displayCurrent.textContent = '';
+    }
+    if(operatorActive == true){
+        displayCurrent.textContent = number;
+        operatorActive = false;
+        return;
+    }
+    displayCurrent.textContent += number;
+    currentNumber += number;
+}
+
+function setOperator(operation){
+    displayOutput.textContent += displayCurrent.textContent + '' + operation;
+    currentOperation = operation;
+    perviousNumber = currentNumber;
+    currentNumber = '';
+    operatorActive = true;
+}
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        setNumber(button.textContent);
+    });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        setOperator(button.textContent);
+    });
+});
